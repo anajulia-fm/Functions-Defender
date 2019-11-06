9e71d28579b7106f3a31550f654d8a39570c3040
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2019-11-06 19:41:49 -0300

    CYCLE_MAP, STATUS: ESTABLE, VERSION: 1.1, OBS.: MINOR IMPROVEMENTS, COMMENTED

--- CHARACTER-STRUCT/struct-character.c
+++ CHARACTER-STRUCT/struct-character.c
@@ -8,16 +8,16 @@ typedef struct{
     int xasis, yasis, lifes, points;
     /*
         Onde:
-         - int xasis -> Posição X do jogador
-         - int yasis -> Posiçãp Y do jogador
-         - int lifes -> Numero de vidas do jogador
-         - int points -> Score do jogador
-         - char ship -> Representação do jogador
+          - int xasis -> Posição X do jogador
+          - int yasis -> Posiçãp Y do jogador
+          - int lifes -> Numero de vidas do jogador
+          - int points -> Score do jogador
+          - char ship -> Representação do jogador
     */
 }player_t;
 
 void mk_player(player_t *p); // Função que define o jogador
-void ptr_player(player_t p); // Função que imprimie o jogador
+void ptr_player(player_t p); // Função que imprime o jogador
 
 int main(){
     player_t player;
@@ -30,15 +30,16 @@ int main(){
 }
 
 void mk_player(player_t *p){
-    // Valoresd de exemplo:
-    p->xasis=1;
-    p->yasis=2;
+    // Valores de exemplo:
+    p->xasis=0;
+    p->yasis=0;
     p->lifes=3;
-    p->points=100;
+    p->points=0;
     strncpy(p->ship[0], "@", 5);
     strncpy(p->ship[1], "@@@@", 5);
 }
 
+// Função de impressão, meramente para vizualização!!
 void ptr_player(player_t p){
     int i;
     printf("\n Eixo X: %d, Eixo Y: %d, Vidas: %d, Pontos: %d\n\n", p.xasis, p.yasis, p.lifes, p.points);
