97a5f4bf22c13b67763bded3fe2a9120b8c1f739
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2019-11-08 19:21:51 -0300

    Branch: CycleMap, Status: Done, -V: 3.0.0

--- CYCLE-MAP/print.c
+++ CYCLE-MAP/print.c
@@ -1,81 +1,75 @@
 #include<stdio.h>
 #include<conio2.h>
 #include<conio.h>
-#include<time.h>
 
 #define MXPTR 5   // Limite de impressão
 #define MXJU 450 // Valor maximo do J da matrizUniverso
 #define MXIU 3  //  Valor maximo do I da matrizUniverso
 
-void mk_u(int matrizUniverso[][MXJU]);
+int ptr_map(int matrizUniverso[][MXJU], int x);
 
 int main(){
-    int matrizUniverso[MXIU][MXJU];
+    // Variaveis de visualizacao:
+    int x=0, endgame=0, aux=0;
+    char c;
 
-    // Variavel que aloca o mapa a ser repitido, neste exemplo '99'
-    // representa o char 'C'
-    int mapaDoArquivoBinario[3][5]={{1, 2, 3, 4, 5},{0, 0, 0, 0, 99}, {1, 2, 3, 4, 5}};
-    
-    int i, j, k, l, y;
+    // Variaveis da function:
+    int matrizUniverso[MXIU][MXJU]; // Matriz universo
+    // Neste exemplo '99' representa o char 'C', que indica que o mapa devera ser repitido
+    int mapaDoArquivoBinario[3][5]={{1, 2, 3, 4, 5},{0, 0, 0, 0, 99}, {1, 2, 3, 4, 5}};// Matriz que aloca o mapa a ser repitido,
+    int i, j; // (x, y) da matriz Universo
+    int k, l; // (x, y) da matriz lida do arquivo
+    int horaDoLoop=0;
 
+    // Function:
+    for(j=0, l=0; j<MXJU;j++, l++){
+        if(horaDoLoop){
+            l=0;
+        }
+        horaDoLoop=0;
+        for(i=0, k=0;i<3;i++, k++){
+            if(mapaDoArquivoBinario[k][l]==99){
+                matrizUniverso[i][j]=0;
+                horaDoLoop=1;
+            }else{
+                matrizUniverso[i][j]=mapaDoArquivoBinario[k][l];
+            }
+        }
+    }
 
-    int x=0, horaDoLoop=0, endgame=0, flagL=0, tempL=0;
-
-    // As variaveis abaixo são utilizadas UNICAMENTE para automatizar o processo
-    // de visualização, totalmente descartáveis...
-    long double start_t, end_t, total_t=0;
-
-    mk_u(u);
+    // So para visualização!
+    ptr_map(matrizUniverso, x);
     while(!endgame){
-        start_t=(double)clock()/CLOCKS_PER_SEC;
-        y=0;
-        for(j=0+x, l=tempL; j<MXPTR+x;j++){
-            if(horaDoLoop){
-                l=0;
-            }
-            horaDoLoop=0;
-            for(i=0, k=0;i<3;i++, k++){
-                if(matrizUniverso[i][j]==100){
-                    if(mapaDoArquivoBinario[k][l]==99){
-                        matrizUniverso[i][j]=0;
-                        horaDoLoop=1;
-                    }else{
-                        matrizUniverso[i][j]=mapaDoArquivoBinario[k][l];
-                    }
-                    flagL=1;
-                }
-                gotoxy(y+5, i+5);  
-                printf("%d", matrizUniverso[i][j]);
-            }
-            if(flagL&&!horaDoLoop){
-                l++;
-            }
-            flagL=0;
-            y++;
-        }
-        tempL=l;
-        end_t=(double)clock()/CLOCKS_PER_SEC;
-        total_t+=(end_t-start_t);
-
-        if(total_t>=2){
+        c=getch();
+        if(c=='d'){
             x++;
-            total_t=0;
-            clrscr();
+            c='z';
         }
-        if(MXPTR+x==MXJU){
-            endgame=1;
+        if(x!=aux){
+            endgame=ptr_map(matrizUniverso, x);
+            aux=x;
         }
     }
 
 return 0;
 }
 
-void mk_u(int matrizUniverso[][MXJU]){
-    int i, j;
+int ptr_map(int matrizUniverso[][MXJU], int x){
+    int i, j, flag;
 
-    for(i=0;i<MXIU;i++){
-        for(j=0;j<MXJU;j++){
-            *(matrizUniverso[0]+i*MXJU+j)=100;
+    flag=0;
+    gotoxy(1, 2);
+    if(x+MXPTR!=MXJU){
+        for(i=0; i<MXIU; i++){
+            for(j=0+x; j<MXPTR+x; j++){
+                printf("%d", matrizUniverso[i][j]);
+            }
+            printf("\n");
         }
+    }else{
+        flag=1;
     }
+    gotoxy(1, 10);
+
+    return flag;
 }
