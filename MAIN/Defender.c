14b12f91aaeb35083ded4402a96dc99228f52dab
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2019-11-08 22:44:39 -0300

    Brench: MAIN, status: Adding Structs, OBS.: Main Game

--- MAIN/Defender.c
+++ MAIN/Defender.c
@@ -6,15 +6,17 @@
 #include<math.h>
 
 #define MAXSPEED 1.25
+#define MAXCOORDX 415
 
 // Player:
 typedef struct{  
     char ship[2][5];
-    int xasis, yasis, lifes, points;
+    int staticXAsis, xAsis, yAsis, lifes, points;
     /*
         Onde:
-          - int xasis -> Posição X do jogador
-          - int yasis -> Posiçãp Y do jogador
+          - int staticXAsis -> Posição x inicial do jogador
+          - int xAsis -> Posição X do jogador
+          - int yAsis -> Posiçãp Y do jogador
           - int lifes -> Numero de vidas do jogador
           - int points -> Score do jogador
           - char ship -> Representação do jogador
@@ -22,15 +24,26 @@ typedef struct{
 }player_t;
 
 // Função que define o jogador
-void mk_player(player_t *p);
+void makePlayer(player_t *p);
 // Função que imprime o jogador
-void ptr_player(player_t p);
+void printPlayer(player_t p);
+// Limpa ultima posição do player:
+void clearPlayer(player_t p);
 
 
 
 int main(){
+    // 'PERSONAGENS':
+    player_t player;
+
     // MISCELLANEOUS:
     char c;
+
+    // Temporarias, substituir por leitura de matriz!
+    // Encontrar o char '@' e atribuir as cordenadas;
+    // ex.: player.yAsis=i, de arquivoMapa[i][j]
+    // e player.xAsis=j, da mesma matriz.
+    int firstX, firstY;
     
     // 'FLAGS':
     int dPressedFlag=0, CoolDownFlag=0, CoolDownFlagAux=0;
@@ -44,16 +57,23 @@ int main(){
     long double start_t=0, end_t=0, total_t=0; // TEMPO DO LAÇO
     long double CoolDownEnd_t, CoolDownAux_t, coolDownChecker_t=0; // TEMPO DE COOLDOWN
 
-    
+    makePlayer(&player);
     maxSpeed=MAXSPEED;
+
+
+    firstX=10; // Pegar as cordenadas do char '@' no mapa lido
+    firstY=10; // Pegar as cordenadas do char '@' no mapa lido
+    player.staticXAsis=firstX;
+    player.yAsis=firstY
+
+
     while (1){
         start_t=(double)clock()/CLOCKS_PER_SEC;
         dPressedFlag=0;
         // Leitura de teclado 
         if(kbhit()){
             c=getch();
-            switch (c)
-            {
+            switch (c){
                 // D
                 case 'd':
                 case 'D':
@@ -67,13 +87,21 @@ int main(){
                 case 'w':
                 case 'W':
                 case 72:  // UP-ARROW
-                    
+                    clearPlayer(player);
+                    player.yAsis--;
+                    if(player.yAsis<=1){
+                        player.yAsis=1; //Somar 1 no gotoxy(x, y); y=1 => header
+                    }
                     break;
                 // S
                 case 's':
                 case 'S':
                 case 80: // DOWN-ARROW
-        
+                        clearPlayer(player);
+                        player.yAsis++;
+                        if(player.yAsis>=35){
+                            player.yAsis=35;
+                        }
                     break;
                 // A
                 case 'a':
@@ -123,6 +151,11 @@ int main(){
         // Imprime
         if(speed>=maxSpeed){
             if(maxSpeed!=MAXSPEED){
+                // Incrementa posição x da nave
+                player.xAsis++;
+                if(player.xAsis>=MAXCOORDX){
+                    player.xAsis=MAXCOORDX;
+                }
                 // FUNÇÂO DE IMPRESSAO
             }
             speed=0;
@@ -131,4 +164,30 @@ int main(){
 
 
     return 0;
+}
+
+// PLAYER:
+// Function que limpa a ultima posição do jogador:
+void clearPlayer(player_t p){
+    gotoxy(p.staticXAsis, p.yasis);
+    printf("    ");
+    gotoxy(p.staticXAsis, p.yasis+1);
+    printf("    ");
+}
+// Cria Jogador:
+void makePlayer(player_t *p){
+    p->xasis=0;
+    p->yasis=0;
+    p->lifes=3;
+    p->points=0;
+    strncpy(p->ship[0], "@", 5);
+    strncpy(p->ship[1], "@@@@", 5);
+}
+// Imprime Jogador:
+void printPlayer(player_t p){
+    int i;
+    for(i=0;i<2;i++){
+        gotoxy(p.xasis, p.yasis-i);
+        printf("%s\n", p.ship[i]);
+    }
 }
