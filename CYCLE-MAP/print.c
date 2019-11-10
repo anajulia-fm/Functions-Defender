b3e0c308e7edee6c8e170e5c2fc48be8ce8a045a
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2019-11-10 08:40:08 -0300

    Brench: Struct/CycleMap, status: Done, -V: 2.5.1

--- CYCLE-MAP/print.c
+++ CYCLE-MAP/print.c
@@ -9,91 +9,20 @@
 #define MXJBIN 76
 
 int ptr_map(int matrizUniverso[][MXJU], int x);
+void fillGaps(char mapaDoArquivoBinario[][MXJBIN]);
 
 int main(){
     // Variaveis de visualizacao:
     int x=0, endgame=0, aux=0;
     char c;
-
     // Variaveis da function:
     int matrizUniverso[MXIU][MXJU]; // Matriz universo
     char mapaDoArquivoBinario[MXIU][MXJBIN]={"CC                                                             CCCCCCCCCCCC", " CC                CCCCC                                     CCC           ", "  CCCCCC          CC   CC                               CCCCCC             ", "       CCCC     CCC     CC               CCCC         CCC                  ", "          CCCCCCC        C             CCC  CC      CCC                    ", "                         CCCCCCCCC    CC     CCCCCCCC                      ", "                                 CCCCCC                                    ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "                                                                           ", "           CCCCCCC                                                         ", "   CCCCCCCCC     C                                   C                     ", "CCCC             CC                                CC C                  CC", "                  CCC                             CC  CC                CC ", "                    CC                           CC    CCCCCCCCCCCCCCCCCC  ", "                     CCCCCCCCCCCCCCCCCCCCCCCCCCCCC                         "};
 
     // Rotina FillGaps: Aloca uns nos 'buracos'
-    {
-        // FLAGS:
-        int flag=0, iFlag=0;
-        // AUX:
-        int auxI=0, i=0, j=0;
-
-        j=0;
-        do{
-            flag=0;
-            i=0;
-            do{
-                iFlag=0;
-                if(mapaDoArquivoBinario[i][j]=='C'){
-                    //Parte Superior do Mapa
-                    if(i<=17){
-                        auxI=i-1;
-                        if(auxI<0){
-                            auxI=0;
-                        }else{
-                            auxI=1;
-                        }
-                        // Preenche
-                        if(auxI){
-                            if(mapaDoArquivoBinario[i-1][j]==' '){
-                                for(auxI=i-1;auxI>=0;auxI--){
-                                    mapaDoArquivoBinario[auxI][j]='C';
-                                }
-                            }
-                        }
-
-                    }
-                    //Parte Inferior do Mapa
-                    if(i>=18){
-                        // Testa se não acessa areas de memoria indesejadas
-                        auxI=i+1;
-                        if(auxI>MXIU){
-                            auxI=0;
-                        }else{
-                            auxI=1;
-                        }
-                        if(auxI){
-                            if(mapaDoArquivoBinario[i+1][j]==' '){
-                                for(auxI=i+1;auxI<MXIU;auxI++){
-                                    mapaDoArquivoBinario[auxI][j]='C';
-                                }
-                                iFlag=1;
-                            }
-                        }
-                    }
-                }
-                i++;
-            }while(i<MXIU&&!iFlag);
-            j++;
-        }while(j!=MXJBIN-1);
-    }
+    fillGaps(mapaDoArquivoBinario);
     // Function de Loop:
-    {
-        int i, j; // (x, y) da matriz Universo
-        int k, l; // (x, y) da matriz lida do arquivo
-        int horaDoLoop=0;
-
-        for(j=0, l=0; j<MXJU;j++, l++){
-            if(horaDoLoop){
-                l=0;
-            }
-            horaDoLoop=0;
-            for(i=0, k=0;i<MXIU;i++, k++){
-                if(l==MXJBIN-2){
-                    horaDoLoop=1;
-                }
-                matrizUniverso[i][j]=0+mapaDoArquivoBinario[k][l];
-            }
-        }
-    }
+    makeMap(mapaDoArquivoBinario, matrizUniverso);
     // So para visualização!
     ptr_map(matrizUniverso, x);
     while(!endgame){
@@ -133,4 +62,81 @@ int ptr_map(int matrizUniverso[][MXJU], int x){
     }
 
     return flag;
+}
+
+// Preenche os espaços com uns:
+void fillGaps(char bin[][MXJBIN]){
+    // FLAGS:
+    int flag=0, iFlag=0;
+    // AUX:
+    int auxI=0, i=0, j=0;
+
+    j=0;
+    do{
+        flag=0;
+        i=0;
+        do{
+            iFlag=0;
+            if(bin[i][j]=='C'){
+                //Parte Superior do Mapa
+                if(i<=17){
+                    auxI=i-1;
+                    if(auxI<0){
+                        auxI=0;
+                    }else{
+                        auxI=1;
+                    }
+                    // Preenche
+                    if(auxI){
+                        if(bin[i-1][j]==' '){
+                            for(auxI=i-1;auxI>=0;auxI--){
+                                bin[auxI][j]='C';
+                            }
+                        }
+                    }
+                }
+                //Parte Inferior do Mapa
+                if(i>=18){
+                    // Testa se não acessa areas de memoria indesejadas
+                    auxI=i+1;
+                    if(auxI>MXIU){
+                        auxI=0;
+                    }else{
+                        auxI=1;
+                    }
+                    if(auxI){
+                        if(bin[i+1][j]==' '){
+                            for(auxI=i+1;auxI<MXIU;auxI++){
+                                bin[auxI][j]='C';
+                            }
+                            iFlag=1;
+                        }
+                    }
+                }
+            }
+            i++;
+        }while(i<MXIU&&!iFlag);
+        j++;
+    }while(j!=MXJBIN-1);
+}
+
+// Copia a matriz mapaDoArquivoBinario quantas vezes for necessario
+// até preencher a matrizUniverso - mapa do jogo
+void makeMap(char bin[][MXJBIN], int mapU[][MXJU]){
+int i, j; // (x, y) da matriz Universo
+        int k, l; // (x, y) da matriz lida do arquivo
+        int horaDoLoop=0;
+
+        for(j=0, l=0; j<MXJU;j++, l++){
+            if(horaDoLoop){
+                l=0;
+            }
+            horaDoLoop=0;
+            for(i=0, k=0;i<MXIU;i++, k++){
+                if(l==MXJBIN-2){
+                    horaDoLoop=1;
+                }
+                mapU[i][j]=0+bin[k][l];
+            }
+        }
 }
