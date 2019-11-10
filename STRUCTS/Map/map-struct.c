4ccf8479d9012ced06caa15fd435c403d35301d0
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2019-11-10 11:37:59 -0300

    Brench: STRUCTS/Map, Satus: Done

---
+++ STRUCTS/Map/map-struct.c
@@ -0,0 +1,8 @@
+
+#define MXJU 500 // Valor maximo do J da matrizUniverso
+#define MXIU 35  //  Valor maximo do I da matrizUniverso
+
+typedef struct{
+    int initalX, finalX;
+    int u[MXIU][MXJU];
+}map_t;
